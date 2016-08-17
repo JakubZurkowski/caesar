3b09b5e9fb12e278f65a1d85f5459e09f232c63b
Baueren <b-bajda@o2.pl>
2016-08-17 17:09:41 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,23 @@
 #include <iostream>
-
+#include <string>
 #include "Cezar.h"
 
 using namespace std;
 
 int main() {
-
+	string word;
 	cout << "Cezar Rot 13: " << endl;
 	Cezar c = Cezar();
 
+	cout << "Podaj ciag znakow do zaszyfrowania: ";
+	cin >> word;
+
+	cout << "Podany tekst:" << word << endl;
+	word = c.szyfruj(word);
+	cout << "Zaszyfrowany tekst: " << word << endl;
+
+	word = c.deszyfruj(word);
+	cout << "Rozszyfrowany tekst: " << word << endl;
 
 	cout << "\n\n\n";
 	system("pause");
